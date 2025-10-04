8fa69ae14956b62a4f578736f74bd0560c3d716a
Md. Riajul Alam Bhuiyan <riajulalam5@gmail.com>
2025-10-04 16:43:43 +0600

    Complete Simple Crud functionality

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,118 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        // GET: Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The DisplayOrder can not exactly match the name");
+            //}
+            //if (obj.Name!=null && obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("","test is an invalid value");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET: Create
+        public IActionResult Edit(int? id)
+        {
+            if(id==0 || id == null)
+            {
+                return NotFound();
+            }
+            //Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            //Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category? categoryFromDb = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id =1, Name = "Action", DisplayOrder=1},
+                new Category { Id = 2, Name = "Action", DisplayOrder = 9 },
+                new Category { Id = 3, Name = "Action", DisplayOrder =2 },
+                new Category { Id = 4, Name = "Action", DisplayOrder = 3 },
+                new Category { Id = 5, Name = "Action", DisplayOrder = 4 },
+                new Category { Id = 6, Name = "Action", DisplayOrder = 5 },
+                new Category { Id = 7, Name = "Action", DisplayOrder = 6 },
+                new Category { Id = 8, Name = "Action", DisplayOrder = 7 },
+                new Category { Id = 9, Name = "Action", DisplayOrder = 8 },
+                new Category { Id = 10, Name = "Action", DisplayOrder = 10 }
+
+             );
+        }
+    }
+}
